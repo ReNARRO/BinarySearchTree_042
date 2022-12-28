1628bba88b66e72774a9ab1fa259e9ff0c046d5c
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2022-12-28 17:08:58 +0700

    menambhakan method node dengan variabel i l dan r

--- BinarySearchTree_042/Program.cs
+++ BinarySearchTree_042/Program.cs
@@ -9,9 +9,17 @@ namespace BinarySearchTree_042
     class Node
     {
         public string info;
-        public string leftchild;
-        public string rightchild;
+        public Node leftchild;
+        public Node rightchild;
+
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
     }
+
     internal class Program
     {
         static void Main(string[] args)
